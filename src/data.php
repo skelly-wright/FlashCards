154fd9fcc9dfc94764a78a22a1dc90f190f5c1b3
skelly-wright <sak66330@ucmo.edu>
2023-08-31 20:15:09 -0500

    Added API Function for each part of CRUD. Centered around flashdeck cards.

--- src/data.php
+++ src/data.php
@@ -30,9 +30,17 @@ $cardsets = [
     ]
 ];
 
-function post_card($ID)
+function post_card(Int $cardSetID, String $content)
 {
-
+    global $cardsets;
+    $cardSetID -= 1;
+    $ID = count($cardsets[$cardSetID]["cards"]) + 1;
+    array_push($cardsets[$cardSetID]["cards"],
+        [
+            "ID"=>$ID,
+            "header"=>$content
+        ]
+    );
 }
 
 function post_cardSet($ID)
@@ -59,9 +67,13 @@ function get_cardSet($ID)
 
 }
 
-function put_card($ID)
+function put_card(Int $cardSetID, Int $cardID, String $content)
 {
-
+    global $cardsets;
+    $cardSetID -= 1;
+    $cardID -= 1;
+    $putArray = array("header" => $content);
+	$cardsets[$cardSetID]["cards"][$cardID] = array_merge($cardsets[$cardSetID]["cards"][$cardID], $putArray);
 }
 
 function put_cardSet($ID)
@@ -69,9 +81,26 @@ function put_cardSet($ID)
 
 }
 
-function delete_card($ID)
+function delete_card($cardSetID, $cardID)
 {
-
+    global $cardsets;
+    $i = 0;
+	foreach($cardsets as $cardset)
+	{
+		if($cardset["ID"]==$cardSetID) { 
+            foreach($cardset["cards"] as $card) {
+                if($card["ID"]==$cardID) {
+                    array_splice($cardset["cards"], $cardID-1, 1); 
+                    $i--;
+                };
+                if($card["ID"]>$cardID) {
+                    $cardset["cards"][$i]["ID"]--; 
+                };
+                $i++;
+            }
+            $cardsets[$cardSetID-1]["cards"] = $cardset["cards"];
+        }
+	}
 }
 
 function delete_cardSet($ID)
@@ -79,5 +108,14 @@ function delete_cardSet($ID)
 
 }
 
-echo get_card(1,1) . "\r\n";
+echo get_card(1,1) . ": First card of first set.\n";
+
+post_card(1, "Addition Card");
+echo get_card(1,3) . ": New card for first set.\n";
+
+put_card(1,3,"Subtraction Card");
+echo get_card(1,3) . ": New card edited.\n";
+
+delete_card(1,1);
+echo get_card(1,1) . ": First card of first set deleted. Second card becomes first card.\n";
 ?>
